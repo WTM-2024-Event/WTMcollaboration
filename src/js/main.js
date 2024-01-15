a46a3cfc5f3c45b0224d6bb5a172d2e81238be74
Ainhoadlhs <ainhoasilveira@gmail.com>
2024-01-15 23:57:39 +0100

    changes header

--- src/js/main.js
+++ src/js/main.js
@@ -6,5 +6,6 @@ const itemsContainer = document.querySelector('.navbar_items');
 
 toggleButton.onclick = () => {
     itemsContainer.classList.toggle("open");
+
     toggleButton.classList.toggle("close");
 };

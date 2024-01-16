868678e8e6f19f3580891c805cec8be85272d69c
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2024-01-16 18:55:19 +0100

    added modal

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,22 @@ const toggleButton = document.querySelector('.nav_toggle');
 const itemsContainer = document.querySelector('.navbar_items');
 
 toggleButton.onclick = () => {
-    itemsContainer.classList.toggle('open');
+  itemsContainer.classList.toggle('open');
 
-    toggleButton.classList.toggle('close');
+  toggleButton.classList.toggle('close');
 };
 
+// Modales Ponentes
+const openModal = document.querySelector('.js-modal');
+const closeModal = document.querySelector('.js-close');
+const modal = document.querySelector('.js-modalPaula');
+
+openModal.addEventListener('click', (event) => {
+  event.preventDefault();
+  modal.classList.remove('hidden');
+});
+
+closeModal.addEventListener('click', (event) =>{
+    event.preventDefault();
+    modal.classList.add('hidden');
+})
